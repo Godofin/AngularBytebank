725d6ff66a7fa415782681cb57e1752ca3a8007b
Guilherme Lavezzo <guilherm909088@gmail.com>
2022-06-23 18:22:38 -0300

    Criando primeiro componente

---
+++ Bytebank/src/app/nova-tranferencia/nova-tranferencia.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NovaTranferenciaComponent } from './nova-tranferencia.component';
+
+describe('NovaTranferenciaComponent', () => {
+  let component: NovaTranferenciaComponent;
+  let fixture: ComponentFixture<NovaTranferenciaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NovaTranferenciaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NovaTranferenciaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
